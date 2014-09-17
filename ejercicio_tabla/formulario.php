fc7c2bffab0b76f1ab097222762d3a3b137263d8
Borjamunoz <munoz.borja@gmail.com>
2014-09-17 10:45:11 +0200

    nuevo

---
+++ ejercicio_tabla/formulario.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+    <?php 
+    	echo "<table border=1>";
+			for ($x=0; $x<$_GET["num"]; $x++){
+	  			echo "<tr>
+	  					<td>aaa</td><td>bbb</td>
+	  				 </tr>";
+	  		}
+  		echo "</table>";
+    ?>
+    
+
+</body>
+</html>
