d9e55094416d70d59dfb5855e3308b37bbddbb27
Borjamunoz <munoz.borja@gmail.com>
2014-09-18 11:43:45 +0200

    correction

--- ejercicio_tabla/formulario.php
+++ ejercicio_tabla/formulario.php
@@ -1,25 +1,32 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
 <html>
 <body>
-
     <?php
-  		if (empty($_GET["num"])) {
-		    echo "No puedes dejarlo vacío";
-		}else if(is_numeric($_GET["num"])){
-	    	if($num<=0){
-	    		echo "no puedes introducir negativos o 0";
-	    	}
-	    	$num = $_GET["num"]; 
-	    	echo "<table border=1>";
-				for ($x=0; $x<$num; $x++){
-		  			echo "<tr>
-		  					<td>aaa</td><td>bbb</td>
-		  				 </tr>";
-		  		}
-	  		echo "</table>";
-		}else{
-			echo "Debes introducir un número";
-		} 
-    ?>
+    	$num = $_GET["num"];
 
+  		if (empty($num)) {
+			
+			echo "No puedes dejarlo vacío";
+			
+		}else if(is_numeric($_GET["num"])){
+
+			if($num<=0 || strpos($num, '.')!==false){
+				echo "no puedes introducir negativos o 0 o decimales";
+			}else{
+
+				echo "<table border=1>";
+				for ($x=0; $x<$num; $x++){
+					echo "<tr>
+					<td>aaa</td><td>bbb</td>
+					</tr>";
+			}
+				echo "</table>";
+			}
+
+			}else{
+				echo "Debes introducir un número";
+			}
+    ?>
 </body>
 </html>
